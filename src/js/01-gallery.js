4d5d5c552655bcf2655e8288120161100b392c7b
ElenaLatynina <latynina.elena.v@gmail.com>
2022-08-02 11:28:34 +0300

    css links  changed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,9 +4,10 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const gallery = document.querySelector(`.gallery`);
+let instance;
 
 const createGallery = (galleryItems) => {
-  return galleryItems.map(({ original, preview, description}) => `<div class="gallery__item"> <a class="gallery__link" 
+  return galleryItems.map(({ original, preview, description}) => `<div class='gallery__item'> <a class='gallery__link' 
   href="${original}">
     <img
       class="gallery__image"
@@ -21,15 +22,30 @@ gallery.insertAdjacentHTML('beforeend', createGallery(galleryItems));
 
 const onGalleryItemClick = (event) => {
   event.preventDefault();
-  const instance = basicLightbox.create(`
+  if (event.target.nodeName !== `IMG`) {
+    return;
+  }
+  instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-
+`, {
+  onShow: (instance) => {window.addEventListener('keydown', onEscKeyPress)},
+  onClose: (instance) =>{window.removeEventListener('keydown', onEscKeyPress)}
+})
   instance.show()
 };
 
 gallery.addEventListener(`click`, onGalleryItemClick);
 
+function onModalClose(){
+  instance.close();
+}
+
+function onEscKeyPress(event) {
+  if (event.code === "Escape") {
+    onModalClose();
+    
+  }
+}
 
 
 
